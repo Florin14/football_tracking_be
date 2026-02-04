224af7ba3904f3d1fe6f261ffad67712bb2ca32e
Grigore-Florin Zimbru <95249603+Florin14@users.noreply.github.com>
2026-02-04 20:18:01 +0200

    FZ/fix

--- src/modules/match/routes/get_match.py
+++ src/modules/match/routes/get_match.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session, joinedload
 
 from extensions.sqlalchemy import get_db
 from modules.match.models import (
-    MatchModel, MatchResponse
+    MatchModel, MatchResponse, GoalModel
 )
 from project_helpers.dependencies import GetInstanceFromPath
 from .router import router
@@ -17,10 +17,26 @@ async def get_match(match: MatchModel = Depends(GetInstanceFromPath(MatchModel))
             joinedload(MatchModel.team1),
             joinedload(MatchModel.team2),
             joinedload(MatchModel.league),
+            joinedload(MatchModel.goals).joinedload(GoalModel.player),
+            joinedload(MatchModel.goals).joinedload(GoalModel.team),
         )
         .filter(MatchModel.id == match.id)
         .first()
     )
+
+    goals = []
+    for goal in match.goals or []:
+        goals.append({
+            "id": goal.id,
+            "matchId": goal.matchId,
+            "playerId": goal.playerId,
+            "playerName": goal.player.name if goal.player else "Unknown",
+            "teamId": goal.teamId,
+            "teamName": goal.team.name if goal.team else "Unknown",
+            "minute": goal.minute,
+            "timestamp": goal.timestamp,
+            "description": goal.description,
+        })
 
     return MatchResponse(
         id=match.id,
@@ -32,5 +48,5 @@ async def get_match(match: MatchModel = Depends(GetInstanceFromPath(MatchModel))
         scoreTeam1=match.scoreTeam1,
         scoreTeam2=match.scoreTeam2,
         state=match.state.value,
-        goals=[]
+        goals=goals
     )
